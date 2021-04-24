ee5916b45f19da1daf140dd4c8dcb65c9d8b16eb
ramesh19831 <jirautomation001@gmail.com>
2021-04-24 18:40:01 +0530

    Commit with DEM-19 Code - Loginwith Invalid crednetials

--- example.maven.project/src/test/java/com/demowebshop/testscripts/TC01_LoginTest.java
+++ example.maven.project/src/test/java/com/demowebshop/testscripts/TC01_LoginTest.java
@@ -64,7 +64,7 @@ public class TC01_LoginTest extends BasePage{
 	
 	@Test
 	public void LoginwithInvalidData() {
-		
+		System.out.println("Login with invalid Credentials");
 	}
 	@AfterClass
 	public void closeBrowser() {
